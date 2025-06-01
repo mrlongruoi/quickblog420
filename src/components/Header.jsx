2f022cb964a7440c1c83cd481fb45ba97679a161
mrlongruoi <mrlong01012020@gmail.com>
2025-06-01 23:46:45 +0700

    fix tailwindcss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 
 const Header = () => {
   return (
-    <div className="mx-8 sm:mx-16 xl-:mx-24 relative">
+    <div className="mx-8 sm:mx-16 xl:mx-24 relative">
       <div className="text-center mt-20 mb-8">
         <div className="inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary">
           <p>AI: Tính năng tích hợp mới</p>
